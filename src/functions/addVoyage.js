962a78aa6cc395ad29fdcab65bc254a3f2eba95f
Marc Fielding <marc.fielding@transferwise.com>
2019-11-14 10:57:43 +0000

    Added DB schema.

--- src/functions/addVoyage.js
+++ src/functions/addVoyage.js
@@ -10,11 +10,26 @@ const joiSchema = Joi.object().keys({
 export default async (event, context, callback, utils) => {
 
 	const { vessel, datetime, port, captain } = JSON.parse(event.body)
+
 	const validation = Joi.validate({ vessel, datetime, port, captain }, joiSchema)
 
 	if (validation.error) {
 		return callback(null, utils.responder.validationError(validation.error))
 	}
 
+	const data = {
+		vessel,
+		datetime,
+		port,
+		captain,
+	}
+
+	let [err, rows] = await utils.to(utils.mySQL.query('INSERT INTO voyages SET ?', data))
+
+	if (err) {
+
+		utils.logger.error({ req: event }, `Error inserting voyage: ${err}`)
+		return callback(null, utils.responder.internal())
+	}
 	return callback(null, utils.responder.success())
 }
